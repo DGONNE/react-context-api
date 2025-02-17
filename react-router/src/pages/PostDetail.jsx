007f9e52af9e5bc614c14d1d132dcdf9b54e48a4
Davide Gonnelli <davide.gonnelli1@gmail.com>
2025-02-17 23:51:30 +0100

    implemento alert in dettaglio piatti

--- react-router/src/pages/PostDetail.jsx
+++ react-router/src/pages/PostDetail.jsx
@@ -1,12 +1,14 @@
 import { useParams } from "react-router-dom";
 import { posts } from "../data/posts";
+import { useAlert } from "../context/AlertContext";
 
 const PostDetail = () => {
   const { id } = useParams();
   const post = posts.find((p) => p.id === parseInt(id));
+  const { showAlert } = useAlert();
 
   if (!post) {
-    return <h2>Post non trovato</h2>;
+    return <h2>Piatto non trovato</h2>;
   }
 
   return (
@@ -14,6 +16,9 @@ const PostDetail = () => {
       <h1>{post.name}</h1>
       <p>{post.description}</p>
       <p>{post.price}</p>
+      <button onClick={() => showAlert(`Hai selezionato "${post.title}"!`)}>
+        Mostra Alert
+      </button>
     </div>
   );
 };
